6eb84d31daa04745dd519291a0f945c2065c539b
alexmhh <alexmhh1234@gmail.com>
2022-04-25 18:28:02 +0300

    Add light/dark mode support + add use-local-storage npm library

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import useLocalStorage from 'use-local-storage';
+import { useEffect } from "react";
 
 export default function Home() {
+  const [theme, setTheme] = useLocalStorage('theme', '');
+  useEffect(() => {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(defaultDark ? 'dark' : 'light')
+  }, [setTheme])
   return (
-    <div className={styles.container}>
-     <h2>Welcome to juni.</h2>
+    <div className={styles.container} data-theme={theme}>
+     <h2 className={styles.primaryText}>Welcome to juni.</h2>
     </div>
   )
 }
