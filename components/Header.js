67e984cbd0573f2848dc38ab2e5032e504b4daf8
alexmhh <alexmhh1234@gmail.com>
2022-04-25 19:33:18 +0300

    Header design draft finished

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,30 @@
 import styles from '../styles/Header.module.css';
+import Link from 'next/link'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
     return(
         <div className={styles.container}>
-            <p>Test 1</p>
-            <p>Test 2</p>
-            <FontAwesomeIcon
-                icon={faUser}
-                className={styles.userIcon}
-            />
-
+            <h3>juni.</h3>
+            <Link href="/login">
+                <a className={styles.item}>Login</a>
+            </Link>
+            <Link href="/">
+                <a className={styles.item}>Main page</a>
+            </Link>
+            <Link href="/user-profile">
+                <FontAwesomeIcon
+                    icon={ faUser }
+                    className={styles.userIcon}
+                />
+            </Link>
+            <Link href="/settings">
+                <FontAwesomeIcon
+                    icon={ faCog }
+                    className={styles.userIcon}
+                />
+            </Link>
         </div>
     )
 }
