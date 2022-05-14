17465262f605762b2fdf38fca6bb2d8387393184
alexmhh <alexmhh1234@gmail.com>
2022-05-14 18:03:58 +0300

    Draft design implementation

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/Login.module.css";
 
 export default function Login() {
+    const [theme, setTheme] = useState('');
+    useEffect(() => {
+        const newTheme = localStorage.getItem('theme')
+        setTheme(newTheme)
+    })
+
     const router = useRouter()
     useEffect(() => {
         const authToken = localStorage.getItem('auth-token')
@@ -13,8 +20,14 @@ export default function Login() {
     })
 
     return(
-        <div>
-
+        <div className={styles.container} data-theme={theme}>
+            <p className={styles.primaryText}>Welcome to juni.</p>
+            <input className={styles.textInput} placeholder='Username or Email' />
+            <input className={styles.textInput} placeholder='Password' />
+            <button className={styles.button}>
+                <p className={styles.buttonText}>Login</p>
+            </button>
+            <p className={styles.secondaryText}>Forgot password?</p>
         </div>
     )
 }
